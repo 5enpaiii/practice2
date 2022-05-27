3822bdaf99c55963e25ca719931aff8c4f878f19
5enpaiii <94631854+5enpaiii@users.noreply.github.com>
2022-05-27 21:45:10 +0300

    task12

---
+++ src/test/java/Task_2/DivByZeroException.java
@@ -0,0 +1,10 @@
+package Task_2;
+
+public class DivByZeroException extends Exception {
+
+    public DivByZeroException(String message) {
+        super("My hitted message " + message);
+    }
+
+    public DivByZeroException() {}
+}
---
+++ src/test/java/Task_5/MyFirstTest.java
@@ -0,0 +1,13 @@
+package Task_5;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+    @Test
+    void myFirstTest(){
+        Assert.assertFalse(true);
+    }
+
+}
